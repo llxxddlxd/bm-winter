2ee1737e33bf5f74be8fea7ea84e2ad734440311
llxxddlxd <llxxdd_lxd@126.com>
2018-08-29 18:24:23 +0800

    提交创建账号接口

--- examples/testAccount.py
+++ examples/testAccount.py
@@ -1,9 +1,14 @@
 import sys
 import os
-sys.path.append(os.path.abspath("..") + "/src/account")
-from account import account;
-account = account();
-# account.randomByRandom();
-ret = account.create();
+sys.path.append(os.path.abspath("..") +"/src/account")
+class testAccount:
+    def testCreate(self):
+        from account import account;
+        account = account();
+        ret = account.create();
+        return ret
+
+testAccount = testAccount();
+ret = testAccount.testCreate();
 print ret
 exit;
---
+++ src/model/request/AccountCheckActivatedRequst.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+#author:winter
+#time 201808
+class AccountCheckActivatedRequst:
+    __address = '';#私有变量
+
+
+
